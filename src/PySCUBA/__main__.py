42163ac6d9f724d3f7411d3b37d0c8b8dbfad962
Gregory Giecold <GGiecold@users.noreply.github.com>
2016-06-17 14:27:42 -0400

    Update __main__.py

--- src/PySCUBA/__main__.py
+++ src/PySCUBA/__main__.py
@@ -9,7 +9,6 @@
 # Contact: g.giecold@gmail.com; ggiecold@jimmy.harvard.edu
 
 
-import optparse
 from os import getcwd, path
 import sys
 
@@ -19,13 +18,15 @@ from sklearn.preprocessing import StandardScaler
 
 from .Gap_stats import gap_stats
 from .Preprocessing import cytometry_preprocess, PCR_preprocess, RNASeq_preprocess
-import .PySCUBApp_design as design
+import .PySCUBA_design
 import .SCUBA_core as SCUBA
 
 
 def plot_tree(cluster_indices, parent_clusters, output_directory = None):
     """Display a bifurcation tree.
     """
+
+    import igraph
 
     if output_directory is None:
         output_directory = getcwd()
@@ -93,12 +94,99 @@ def one_to_max(array_in):
     return result
        
 
-class PySCUBApp(QtGui, QMainWindow, design.Ui_MainWindow):
-    
-    def __init__(self, parent=None):
-        super(PySCUBApp, self).__init__(parent)
-        self.setupUi(self)
+class WorkerThread(QtCore.QThread):
+
+    def __init__(self, data_type, data_path, cluster_mode, log_mode,
+                 pseudotime_mode, pcv_method, anchor_gene,
+                 exclude_marker_names):
+        super(WorkerThread, self).__init__()
         
+        self.data_type = str(data_type)
+        self.data_path = data_path
+        
+        cluster_mode = str(cluster_mode).lower()
+        self.cluster_mode = None if (cluster_mode == 'none') else cluster_mode
+            
+        self.log_mode = log_mode
+        self.pseudotime_mode = pseudotime_mode
+        self.pcv_method = pcv_method
+        self.anchor_gene = anchor_gene
+        self.exclude_marker_names = exclude_marker_names
+        
+    def __del__(self):
+        self.wait()
+        
+    def run(self):
+        preprocessing_fcts = [cytometry_preprocess, PCR_preprocess,
+                          RNASeq_preprocess]
+        data_type_dict = {'cytometry': 0, 'PCR': 1, 'RNASeq': 2}
+        cell_IDs, data, markers, cell_stages, data_tag, \
+        output_directory = preprocessing_fcts[data_type_dict[self.data_type]](
+            self.data_path, self.log_mode, self.pseudotime_mode,
+            self.pcv_method, self.anchor_gene, self.exclude_marker_names)
+    
+        cell_stages = 1 + one_to_max(cell_stages)
+        data = StandardScaler(with_std = False).fit_transform(data)
+    
+        if self.cluster_mode in {'pca', 'pca2'}:    
+            PCA_components, data = SCUBA.PCA_analysis(data, self.cluster_mode,
+                cell_stages if (self.cluster_mode == 'pca2') else None)
+    
+        centroid_coords, cluster_indices, \
+        parent_clusters = SCUBA.initialize_tree(data, cell_stages)
+        centroid_coords, cluster_indices, \
+        parent_clusters = SCUBA.refine_tree(data, centroid_coords,
+            cluster_indices, parent_clusters, cell_stages, output_directory)
+    
+        try:
+            plot_tree(cluster_indices, parent_clusters, output_directory)
+        except ImportError, e:
+            pass
+        
+        if self.cluster_mode in {'pca', 'pca2'}:
+            weights = PCA_components
+        else:
+            weights = None
+    
+        bifurcation_info, bifurcation_axes, \
+        bifurcation_projections = SCUBA.bifurcation_direction(data, cell_IDs,
+            markers, parent_clusters, centroid_coords, output_directory,
+            weights)
+                
+        if bifurcation_info:
+            data_per_split, parameters_per_split = SCUBA.bifurcation_analysis(
+                cluster_indices, bifurcation_info, bifurcation_axes,
+                bifurcation_projections, output_directory)
+        
+        return
+
+
+class PySCUBApp(QtGui.QMainWindow, PySCUBA_design.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.setupUi(self)
+
+        self.data_path = './'
+        self.selectDatasetButton.clicked.connect(self.selectDataset)
+        
+        self.log_mode = True
+        self.logCheckBox.stateChanged.connect(self.logStateChanged)
+        
+        self.pseudotime_mode = True
+        self.pseudotimeCheckBox.stateChanged.connect(
+            self.pseudotimeStateChanged)
+        
+        self.pcv_method = 'Rprincurve' 
+        self.anchor_gene = None 
+        self.exclude_marker_names = None
+        
+        self.okButton.clicked.connect(self.buttonClicked)
+        self.okButton.clicked.connect(self.OK)
+        self.cancelButton.clicked.connect(self.buttonClicked)
+        
+        self.displayFileButton.clicked.connect(self.selectDisplay)
+
     def closeEvent(self, event):
         reply = QtGui.QMessageBox.question(self, 'Message', 
             "Are you sure to quit?", QtGui.QMessageBox.Yes 
@@ -107,66 +195,76 @@ class PySCUBApp(QtGui, QMainWindow, design.Ui_MainWindow):
             event.accept()
         else:
             event.ignore()
-            
+
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Escape:
             self.close()
-       
-              
-def main():
-        
-    opts, file_path = parser.parse_args()
 
-    preprocessing_fcts = [cytometry_preprocess, PCR_preprocess, RNASeq_preprocess]
-    data_type = {'cytometry': 0, 'PCR': 1, 'RNASeq': 2}
-    cell_IDs, data, markers, cell_stages, data_tag, output_directory = preprocessing_fcts[data_type[
-        opts.data_type]](file_path, opts.log_mode, opts.pseudotime_mode, 
-        opts.pcv_method, opts.anchor_gene, opts.exclude_marker_names)
+    def selectDataset(self):
+        self.dataFileDialog = QtGui.QFileDialog(self)
+        self.data_path = str(self.dataFileDialog.getOpenFileName())
+        self.statusbar.showMessage("{0} ready to be "
+            "analyzed".format(path.basename(self.data_path)))
     
-    cell_stages = 1 + one_to_max(cell_stages)
-    data = StandardScaler(with_std = False).fit_transform(data)
+    def logStateChanged(self, int):
+        if self.logCheckBox.isChecked():
+            self.log_mode = True
+        else:
+            self.log_mode = False
     
-    cluster_mode = opts.cluster_mode
+    def pseudotimeStateChanged(self, int):
+        if self.pseudotimeCheckBox.isChecked():
+            self.pseudotime_mode = True
+        else:
+            self.pseudotime_mode = False
     
-    if cluster_mode in {'pca', 'pca2'}:    
-        PCA_components, data = SCUBA.PCA_analysis(data, cluster_mode, 
-                    cell_stages if (cluster_mode == 'pca2') else None)
+    def buttonClicked(self):
+        sender = self.sender()
+        self.statusbar.showMessage(sender.text() + " was pressed.")
+        self.button_clicked = sender.text()
+        
+    def OK(self):
+        self.statusbar.showMessage('Work in progress...')
+        
+        self.get_thread = WorkerThread(self.dataTypeComboBox.currentText(),
+            self.data_path, self.clusterModeComboBox.currentText(),
+            self.log_mode, self.pseudotime_mode, self.pcv_method,
+            self.anchor_gene, self.exclude_marker_names)
+        self.connect(self.get_thread, QtCore.SIGNAL("update(QString)"),
+            self.get_thread.run)
+        self.connect(self.get_thread, QtCore.SIGNAL("finished()"), self.done)
+        self.get_thread.start()
+        self.cancelButton.setEnabled(True)
+        self.okButton.setEnabled(False)
     
-    centroid_coords, cluster_indices, parent_clusters = SCUBA.initialize_tree(data, cell_stages)
-    centroid_coords, cluster_indices, parent_clusters = SCUBA.refine_tree(data, 
-        centroid_coords, cluster_indices, parent_clusters, cell_stages, 
-        output_directory)
+    def done(self):
+        if self.button_clicked == 'Cancel':
+            self.cancel()
+        else:
+            self.cancelButton.setEnabled(False)
+            self.okButton.setEnabled(True)
+            self.statusbar.showMessage("PySCUBA has completed the "
+                "analysis of your data.")
+            QtGui.QMessageBox.information(self, "Status Message", 
+                "Mission completed!")
+            
+    def cancel(self):
+        self.get_thread.terminate()
+        self.statusbar.showMessage("PySCUBA was interrupted!")
+        QtGui.QMessageBox.information(self, "Status Message", 
+            "PySCUBA was interrupted!")
     
-    try:
-        import igraph
-        plot_tree(cluster_indices, parent_clusters, output_directory)
-    except ImportError, e:
+    def selectDisplay(self):
         pass
-        
-    if cluster_mode in {'pca', 'pca2'}:
-        weights = PCA_components
-    else:
-        weights = None
-    
-    bifurcation_info, bifurcation_axes, bifurcation_projections = SCUBA.bifurcation_direction(
-        data, cell_IDs, markers, parent_clusters, centroid_coords, output_directory, weights)
-                
-    if bifurcation_info:
-        data_per_split, parameters_per_split = SCUBA.bifurcation_analysis(
-            cluster_indices, bifurcation_info, bifurcation_axes, 
-            bifurcation_projections, output_directory)
-    
 
-#def main():
-#    
-#    app = QtGui.OApplication(sys.argv)
-#    form = PySCUBApp()
-#    form.show()
-#    sys.exit(app.exec_())
 
+def main():
+    app = QtGui.QApplication(sys.argv)
+    form = PySCUBApp()
+    form.show()
+    sys.exit(app.exec_())
+    
 
 if __name__ == '__main__':
-
     main()
-    
     
