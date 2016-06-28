c05fae405d57cac1ce3e5d5ba5c7e6d47ee25184
Gregory Giecold <GGiecold@users.noreply.github.com>
2016-06-28 13:46:33 -0400

    Add files via upload

---
+++ __version__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+# PySCUBA/__version__.py
+
+
+# Author: Gregory Giecold for the GC Yuan Lab
+# Affiliation: Harvard University
+# Contact: g.giecold@gmail.com; ggiecold@jimmy.harvard.edu
+
+
+__version__ = '0.1.0'
+
+
